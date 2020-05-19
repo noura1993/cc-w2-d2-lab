86f70de3877568e9a0773cd15a53222c1a76a6b3
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-19 19:11:25 +0100

    Pick up and count passengers

--- bus.rb
+++ bus.rb
@@ -1,14 +1,22 @@
 class Bus
-
-    attr_reader :route_number, :destination
+    attr_reader :route_number, :destination, :passengers
 
     def initialize(route_number, destination)
         @route_number = route_number
         @destination = destination
+        @passengers = []
     end
 
     def drive
         return "Brum brum"
+    end
+
+    def pick_up(passenger)
+        @passengers.push(passenger)
+    end
+
+    def count_passengers()
+         return @passengers.length
     end
 
 end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -1,6 +1,7 @@
 require('minitest/autorun')
 require('minitest/reporters')
-require_relative('../bus.rb')
+require_relative('../bus')
+require_relative('../person')
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
@@ -8,6 +9,7 @@ class BusTest < MiniTest::Test
 
     def setup()
         @bus = Bus.new(22, "Ocean Terminal")
+        @person = Person.new("Alice", 29)
     end
 
     def test_create_bus_class()
@@ -17,6 +19,16 @@ class BusTest < MiniTest::Test
 
     def test_drive()
        assert_equal("Brum brum", @bus.drive) 
+    end
+
+    def test_pick_up()
+        @bus.pick_up(@person)
+        assert_equal([@person], @bus.passengers)
+    end
+
+    def test_count_passengers()
+        result = @bus.count_passengers()
+        assert_equal(0, result)
     end
 
 end
